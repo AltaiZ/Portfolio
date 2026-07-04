505b5be754bd8b15c42545aca83f83c45e1219ec
AltaiZ <munkh_altai@yahoo.com>
2026-07-04 22:07:24 +0800

    done

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,11 +12,11 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="relative flex h-dvh items-center justify-center overflow-hidden bg-[url(/76239.jpg)] bg-cover bg-center bg-no-repeat pb-24 sm:pb-28">
+    <div className="relative flex h-dvh items-center justify-center overflow-hidden bg-[url(/76239.jpg)] bg-cover bg-center bg-no-repeat px-4 pb-24 pt-8 sm:pb-28 sm:pt-10">
       <div className="absolute inset-0 bg-black/50" />
-      <div className="relative flex h-full w-full max-w-[50rem] justify-center bg-[url(/portrait23.png)] bg-contain bg-center bg-no-repeat sm:bg-cover">
+      <div className="relative flex h-full w-full max-w-[46rem] justify-center bg-[url(/portrait23.png)] bg-contain bg-center bg-no-repeat">
         <TextLoop
-          className="absolute top-[14dvh] z-10 px-4 text-center font-mono text-2xl font-bold text-card sm:top-[16dvh] md:text-4xl lg:text-6xl"
+          className="absolute top-[13%] z-10 px-4 text-center font-mono text-2xl font-bold text-card sm:top-[14%] md:text-4xl lg:text-6xl"
           trigger={trigger}
         >
           <span>Hello?</span>
